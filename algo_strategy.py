ffac13da17aa1c3e1f7d74a92efc4a8d984830a5
Jaron <jierui1998@gmail.com>
2019-10-02 17:27:01 -0700

    Finished parse_game_state for input. Working on parsing the serialized string directly. Need to move to desktop.

--- algo_strategy.py
+++ algo_strategy.py
@@ -30,6 +30,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         gamelib.debug_write('Random seed: {}'.format(seed))
         self.model = ActorCritic()
         self.model.load_state_dict(torch.load(file_path))
+        self.actions = []
         
     def on_game_start(self, config):
         """ 
@@ -37,7 +38,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         gamelib.debug_write('Configuring your custom algo strategy...')
         self.config = config
-        global FILTER, ENCRYPTOR, DESTRUCTOR, PING, EMP, SCRAMBLER
+        global FILTER, ENCRYPTOR, DESTRUCTOR, PING, EMP, SCRAMBLER, PIECE_TO_INT
         FILTER = config["unitInformation"][0]["shorthand"]
         ENCRYPTOR = config["unitInformation"][1]["shorthand"]
         DESTRUCTOR = config["unitInformation"][2]["shorthand"]
@@ -45,12 +46,96 @@ class AlgoStrategy(gamelib.AlgoCore):
         EMP = config["unitInformation"][4]["shorthand"]
         SCRAMBLER = config["unitInformation"][5]["shorthand"]
         # This is a good place to do initial setup
-        self.scored_on_locations = []        
-        
-    
-    def convert_gamestate_to_input(game_state):
+        self.scored_on_locations = []
 
-        return None
+        PIECE_TO_INT = {FILTER: 1, ENCRYPTOR: 2, DESTRUCTOR: 3, PING: 4, EMP: 5, SCRAMBLER: 6}
+
+    def update_board(board, units, player_number):
+        for i, unit_types in enumerate(units):
+            for uinfo in unit_types:
+                sx, sy, shp = uinfo[:3]
+                x, y = map(int, [sx, sy])
+                hp = float(shp)
+                if board[x, y, 0] == 0:
+                    board[x, y, 0] = i + 1
+                    board[x, y, 1] = hp
+                    board[x, y, 2] = player_number + 1
+
+                
+                # This depends on RM always being the last type to be processed
+                if unit_type == REMOVE:
+                    self.game_map[x,y][0].pending_removal = True
+
+                
+                unit = GameUnit(unit_type, self.config, player_number, hp, x, y)
+                self.game_map[x,y].append(unit)
+    
+    def parse_serialized_string(serialized_string):
+        state = json.loads(serialized_string)
+        turn_info = state["turnInfo"]
+        global PIECE_TO_INT
+        
+        # Input game state as a matrix.
+        # Channel 0: Piece type (0) for empty, (1, 2, 3) for stationary, (4,5,6) for moving pieces
+        # Channel 1: Piece HP
+        # Channel 2: Piece Side [0 = empty, 1 = Friendly, 2 = Enemy]
+        # Channel 3: Amount of Pieces (-1 = marked for removal)
+        board = np.zeros((28, 28, 4))
+        p1units = state["p1Units"]
+        p2units = state["p2Units"]
+        
+
+        # Formats relevant game information in gamedata
+        gamedata = np.zeros(7)
+        p1_health, p1_cores, p1_bits, p1_time = map(float, state["p1Stats"][:4])
+        p2_health, p2_cores, p2_bits, p2_time = map(float, state["p2Stats"][:4])
+        gamedata[0] = game_state.p1_health
+        gamedata[1] = game_state.p1_cores
+        gamedata[2] = game_state.p1_bits
+        gamedata[3] = game_state.p2_health
+        gamedata[4] = game_state.p2_cores
+        gamedata[5] = game_state.p2_bits
+        gamedata[6] = int(turn_info[1])
+
+        return board, gamedata
+        
+
+        
+    def parse_gamestate(game_state):
+        board_width, board_height = 28, 28
+        game_map = game_state.game_map
+        # Input game state as a matrix.
+        # Channel 0: Piece type (0) for empty, (1, 2, 3) for stationary, (4,5,6) for moving pieces
+        # Channel 1: Piece HP
+        # Channel 2: Piece Side [0 = empty, 1 = Friendly, 2 = Enemy]
+        # Channel 3: Amount of Pieces (-1 = marked for removal)
+        board = np.zeros((board_width, board_height, 4))
+        for i in range(board_width):
+            for j in range(board_height):
+                current_list = game_map[i, j]
+
+                if len(current_list) != 0:
+                    hp_total = 0
+                    for unit in current_list:
+                        hp_total += unit.stability
+                    # Assign the correct unit type
+                    unit = current_list[0]
+                    board[i, j, 1] = hp_total
+                    board[i, j, 2] = unit.player_index + 1
+                    board[i, j, 3] = len(current_list)
+                    board[i, j, 0] = PIECE_TO_INT[unit.unit_type]        
+                    
+        # HP, Cores, Bits of both players.
+        gamedata = np.zeros(7)
+        gamedata[0] = game_state.my_health
+        gamedata[1] = game_state._player_resources[0]['cores']
+        gamedata[2] = game_state._player_resources[0]['bits'
+        gamedata[3] = game_state.enemy_health
+        gamedata[4] = game_state._player_resources[1]['cores']
+        gamedata[5] = game_state._player_resources[1]['bits']
+        metadata[6] = game_state.turn_number
+        return board, gamedata
+                                                      
     
     def perform_action_using_output(output, game_state):
 
@@ -68,153 +153,10 @@ class AlgoStrategy(gamelib.AlgoCore):
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
         game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
 
-        self.starter_strategy(game_state)
 
         game_state.submit_turn()
 
 
-    """
-    NOTE: All the methods after this point are part of the sample starter-algo
-    strategy and can safely be replaced for your custom algo.
-    """
-
-
-    def starter_strategy(self, game_state):
-        """
-        For defense we will use a spread out layout and some Scramblers early on.
-        We will place destructors near locations the opponent managed to score on.
-        For offense we will use long range EMPs if they place stationary units near the enemy's front.
-        If there are no stationary units to attack in the front, we will send Pings to try and score quickly.
-        """
-        game_state.attempt_spawn(EMP, [24, 10], 3)
-        # First, place basic defenses
-        self.build_defences(game_state)
-        # Now build reactive defenses based on where the enemy scored
-        self.build_reactive_defense(game_state)
-
-        # If the turn is less than 5, stall with Scramblers and wait to see enemy's base
-        if game_state.turn_number < 5:
-            self.stall_with_scramblers(game_state)
-        else:
-            # Now let's analyze the enemy base to see where their defenses are concentrated.
-            # If they have many units in the front we can build a line for our EMPs to attack them at long range.
-            if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
-                self.emp_line_strategy(game_state)
-            else:
-                # They don't have many units in the front so lets figure out their least defended area and send Pings there.
-
-                # Only spawn Ping's every other turn
-                # Sending more at once is better since attacks can only hit a single ping at a time
-                if game_state.turn_number % 2 == 1:
-                    # To simplify we will just check sending them from back left and right
-                    ping_spawn_location_options = [[13, 0], [14, 0]]
-                    best_location = self.least_damage_spawn_location(game_state, ping_spawn_location_options)
-                    game_state.attempt_spawn(PING, best_location, 1000)
-
-                # Lastly, if we have spare cores, let's build some Encryptors to boost our Pings' health.
-                encryptor_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
-                game_state.attempt_spawn(ENCRYPTOR, encryptor_locations)
-
-    def build_defences(self, game_state):
-        """
-        Build basic defenses using hardcoded locations.
-        Remember to defend corners and avoid placing units in the front where enemy EMPs can attack them.
-        """
-        # Useful tool for setting up your base locations: https://www.kevinbai.design/terminal-map-maker
-        # More community tools available at: https://terminal.c1games.com/rules#Download
-
-        # Place destructors that attack enemy units
-        destructor_locations = [[0, 13], [27, 13], [8, 11], [19, 11], [13, 11], [14, 11]]
-        # attempt_spawn will try to spawn units if we have resources, and will check if a blocking unit is already there
-        game_state.attempt_spawn(DESTRUCTOR, destructor_locations)
-        
-        # Place filters in front of destructors to soak up damage for them
-        filter_locations = [[8, 12], [19, 12]]
-        game_state.attempt_spawn(FILTER, filter_locations)
-
-    def build_reactive_defense(self, game_state):
-        """
-        This function builds reactive defenses based on where the enemy scored on us from.
-        We can track where the opponent scored by looking at events in action frames 
-        as shown in the on_action_frame function
-        """
-        for location in self.scored_on_locations:
-            # Build destructor one space above so that it doesn't block our own edge spawn locations
-            build_location = [location[0], location[1]+1]
-            game_state.attempt_spawn(DESTRUCTOR, build_location)
-
-    def stall_with_scramblers(self, game_state):
-        """
-        Send out Scramblers at random locations to defend our base from enemy moving units.
-        """
-        # We can spawn moving units on our edges so a list of all our edge locations
-        friendly_edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
-        
-        # Remove locations that are blocked by our own firewalls 
-        # since we can't deploy units there.
-        deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
-        
-        # While we have remaining bits to spend lets send out scramblers randomly.
-        while game_state.get_resource(game_state.BITS) >= game_state.type_cost(SCRAMBLER) and len(deploy_locations) > 0:
-            # Choose a random deploy location.
-            deploy_index = random.randint(0, len(deploy_locations) - 1)
-            deploy_location = deploy_locations[deploy_index]
-            
-            game_state.attempt_spawn(SCRAMBLER, deploy_location)
-            """
-            We don't have to remove the location since multiple information 
-            units can occupy the same space.
-            """
-
-    def emp_line_strategy(self, game_state):
-        """
-        Build a line of the cheapest stationary unit so our EMP's can attack from long range.
-        """
-        # First let's figure out the cheapest unit
-        # We could just check the game rules, but this demonstrates how to use the GameUnit class
-        stationary_units = [FILTER, DESTRUCTOR, ENCRYPTOR]
-        cheapest_unit = FILTER
-        for unit in stationary_units:
-            unit_class = gamelib.GameUnit(unit, game_state.config)
-            if unit_class.cost < gamelib.GameUnit(cheapest_unit, game_state.config).cost:
-                cheapest_unit = unit
-
-        # Now let's build out a line of stationary units. This will prevent our EMPs from running into the enemy base.
-        # Instead they will stay at the perfect distance to attack the front two rows of the enemy base.
-        for x in range(27, 5, -1):
-            game_state.attempt_spawn(cheapest_unit, [x, 11])
-
-        # Now spawn EMPs next to the line
-        # By asking attempt_spawn to spawn 1000 units, it will essentially spawn as many as we have resources for
-        game_state.attempt_spawn(EMP, [24, 10], 1000)
-
-    def least_damage_spawn_location(self, game_state, location_options):
-        """
-        This function will help us guess which location is the safest to spawn moving units from.
-        It gets the path the unit will take then checks locations on that path to 
-        estimate the path's damage risk.
-        """
-        damages = []
-        # Get the damage estimate each path will take
-        for location in location_options:
-            path = game_state.find_path_to_edge(location)
-            damage = 0
-            for path_location in path:
-                # Get number of enemy destructors that can attack the final location and multiply by destructor damage
-                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(DESTRUCTOR, game_state.config).damage
-            damages.append(damage)
-        
-        # Now just return the location that takes the least damage
-        return location_options[damages.index(min(damages))]
-
-    def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
-        total_units = 0
-        for location in game_state.game_map:
-            if game_state.contains_stationary_unit(location):
-                for unit in game_state.game_map[location]:
-                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
-                        total_units += 1
-        return total_units
         
     def filter_blocked_locations(self, locations, game_state):
         filtered = []
